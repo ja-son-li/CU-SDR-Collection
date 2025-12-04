a29225ae4a030147571eb4708f28afd9c821966a
jasp9644 <jasp9644@colorado.edu>
2025-12-04 11:52:50 -0700

    Add files via upload

--- GAL/GAL_E5a/Automate_SDR_Jason.m
+++ GAL/GAL_E5a/Automate_SDR_Jason.m
@@ -25,16 +25,15 @@ rec_pos = [rec_pos; 1614.541];
 
 % Create a array that will hold the output code corresponding to the data
 % set input (n)
-Testing_Results = zeros(n, 6);
+Testing_Results = zeros(n, 5);
 % Column 1  - Index of File
 % Column 2  - Category Flag [1, 2, 3, or 4]
 % Column 3  - Standard Deviation Precision of Navigation Solution
 % Column 4  - Mean Accuracy of Navigation Error (m)
 % Column 5  - Run Time (s) of init per data file
-% Column 6  - Name of File
 
 %% Begin processing Data Sets
-for i = 1:n
+for i = 1:100
     % Try running init process for data set, n
     fprintf("\n \n ========== %d ========== \n", i)
     try
@@ -54,13 +53,12 @@ for i = 1:n
     load("acqResults.mat")
     load("trkResults.mat")
     load('navResults.mat')
-
+    save(sprintf('file_%d.mat',i),'navResults','trkResult')
     if Testing_Results(i, 2) == 1
         % If there is a error, do nothing
         Testing_Results(i, 3) = NaN;
         Testing_Results(i, 4) = NaN;
         Testing_Results(i, 5) = timeDiff;
-        %Testing_Results(i, 6) = d(i).name;
 
     elseif isempty(navResults) || isnan(mean(sqrt(navResults.X.^2+navResults.Y.^2+navResults.Z.^2)))
         % Check if navigation solution is empty or NAN
@@ -70,7 +68,6 @@ for i = 1:n
         Testing_Results(i, 3) = NaN;
         Testing_Results(i, 4) = NaN;
         Testing_Results(i, 5) = timeDiff;
-        %Testing_Results(i, 6) = d(i).name;
 
     elseif sqrt((mean(navResults.X) - rec_X)^2 + (mean(navResults.Y) - rec_Y)^2 + (mean(navResults.Z) - rec_Z)^2) > 25
         % If position solution - reference is greater than 25 units apart,
@@ -80,7 +77,6 @@ for i = 1:n
         Testing_Results(i, 3) = mean([std(navResults.X), std(navResults.Y), std(navResults.Z)]);
         Testing_Results(i, 4) = sqrt((mean(navResults.X) - rec_X)^2 + (mean(navResults.Y) - rec_Y)^2 + (mean(navResults.Z) - rec_Z)^2);
         Testing_Results(i, 5) = timeDiff;
-        %Testing_Results(i, 6) = d(i).name;
 
     elseif sqrt((mean(navResults.X) - rec_X)^2 + (mean(navResults.Y) - rec_Y)^2 + (mean(navResults.Z) - rec_Z)^2) <= 25
         % If position solution - reference is less than or equal to 25 units apart,
@@ -90,12 +86,11 @@ for i = 1:n
         Testing_Results(i, 3) = mean([std(navResults.X), std(navResults.Y), std(navResults.Z)]);
         Testing_Results(i, 4) = sqrt((mean(navResults.X) - rec_X)^2 + (mean(navResults.Y) - rec_Y)^2 + (mean(navResults.Z) - rec_Z)^2);
         Testing_Results(i, 5) = timeDiff;
-        %Testing_Results(i, 6) = d(i).name;
     end
-
     fprintf("Output Flag %d \n", Testing_Results(i, 2))
     fprintf("Data File Processing is over elapsed time   %7.2f n seconds\n", timeDiff)
-
+    % Saving .mat File
+    save("Testing_Results_SDR.mat");
 end % 1:n Data Sets
 
 %% Post Processing 
@@ -105,44 +100,21 @@ close all;
 edges = unique(Testing_Results(:, 2));
 counts = histc(Testing_Results(:, 2), edges);
 
-% Finding the Best, Worst, and Mean precision Test File in Column 3
-[minPre, minPreFile] = min(Testing_Results(:, 3), [], 'omitNan');
-[maxPre, maxPreFile] = max(Testing_Results(:, 3), [], 'omitNan');
-meanPre = mean(Testing_Results(:, 3), 'omitNan');
+% Finding the Best and Worst Accuracy Test File
+[minErr, minFile] = min(Testing_Results(:, 4), [], 'omitNan');
+[maxErr, maxFile] = max(Testing_Results(:, 4), [], 'omitNan');
 
-% Finding the Best, Worst, and Mean Accuracy Test File in Column 4
-[minAcc, minAccFile] = min(Testing_Results(:, 4), [], 'omitNan');
-[maxAcc, maxAccFile] = max(Testing_Results(:, 4), [], 'omitNan');
-meanAcc = mean(Testing_Results(:, 4), 'omitNan');
-
-% Finding the Faster, Slowest, and Mean Timing Test File in Column 5
-[minTime, minTimeFile] = min(Testing_Results(:, 5), [], 'omitNan');
-[maxTime, maxTimeFile] = max(Testing_Results(:, 5), [], 'omitNan');
-meanTime = mean(Testing_Results(:, 5), 'omitNan');
-
-%% PRINTOUT
-fprintf("\n===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====\n")
 % Category Printout
+fprintf("\n===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====\n")
 for i = 1:length(edges)
     fprintf("     Category %d:    %d    entries \n", edges(i), counts(i));
 end
 
-% Precision Printout
-fprintf("File  #  %i  has the smallest navigation precision:   %7.2f   \n", minPreFile, minPre)
-fprintf("File  #  %i  has the  biggest navigation precision:   %7.2f   \n", maxPreFile, maxPre)
-fprintf("Mean of the Navigation precision:                     %7.2f   \n\n", meanPre)
-
 % Accuracy Printout
-fprintf("File  #  %i  has the smallest navigation error:   %7.2f   meters\n", minAccFile, minAcc)
-fprintf("File  #  %i  has the  biggest navigation error:   %7.2f   meters\n", maxAccFile, maxAcc)
-fprintf("Mean of the Navigation error:                     %7.2f   meters\n\n", meanAcc)
-
-% Timing Printout
-fprintf("File  #  %i  ran the faster:       %7.2f   s\n", minTimeFile, minTime)
-fprintf("File  #  %i  ran the slowest:      %7.2f   s\n", maxTimeFile, maxTime)
-fprintf("Mean File Runtime:                 %7.2f   s\n", meanTime)
+fprintf("File  #  %i  has the smallest navigation error:   %7.2f   meters\n", minFile, minErr)
+fprintf("File  #  %i  has the  biggest navigation error:   %7.2f   meters\n", maxFile, maxErr)
 fprintf("===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====\n")
 
 % Saving .mat File
 save("Testing_Results_SDR.mat");
-save("d.mat")
+
